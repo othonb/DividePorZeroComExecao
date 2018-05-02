3065f44f3a1023ac3f494c1d588e4dacb29527ad
Othon Marcelo Nunes Batista <othon.batista@labinfo.unifacs.br>
2018-05-02 09:58:46 -0300

    Metodo criado para leitura de inteiros

--- src/divideporzerocomexcecao/DividePorZeroComExcecao.java
+++ src/divideporzerocomexcecao/DividePorZeroComExcecao.java
@@ -5,21 +5,64 @@ import java.util.Scanner;
 
 public class DividePorZeroComExcecao {
     
-    public static int quociente (int numerador, int denominador)
+    private static int quocienteComExcecao (int numerador, int denominador)
             throws ArithmeticException {
         
         return numerador / denominador;
         
     }
+    
+    private static int quocienteSemExcecao (int numerador, int denominador) {
+        
+        return numerador / denominador;
+        
+    }
+    
+    private static int leInteiro (String msg) {
+        
+        Scanner scanner = new Scanner (System.in);
+        
+        boolean continuaNoLaco = true;
+        
+        int numero = 0;
+        
+        // Leitura do numerador
+        while (continuaNoLaco) {
+            
+            try {
+                
+                System.out.print (msg);
+                numero = scanner.nextInt();
+                
+                continuaNoLaco = false;
+            
+            } catch (InputMismatchException e) {
+                
+                System.out.println ("Numero invalido! Por favor informe um numero inteiro!!!");
+                
+                scanner.nextLine();
+                
+            } finally {
+                
+                System.out.println ("Com ou sem excecao, esse codigo e executado!!!");
+                
+            }
+            
+        }
+        
+        return numero;
+        
+    }
 
     public static void main(String[] args) {
         
-        Scanner scanner = new Scanner (System.in);
-        boolean continuaNoLaco = true;
+        //Scanner scanner = new Scanner (System.in);
+        //boolean continuaNoLaco = true;
         int numerador = 0;
         int denominador = 0;
         
         // Leitura do numerador
+        /*
         while (continuaNoLaco) {
             
             try {
@@ -39,7 +82,7 @@ public class DividePorZeroComExcecao {
             
         }
         
-        continuaNoLaco = true;
+       continuaNoLaco = true;
         
         // Leitura do denominador
         while (continuaNoLaco) {
@@ -60,10 +103,24 @@ public class DividePorZeroComExcecao {
             }
             
         }
+*/
         
-        try {
+        // Lê o numerador
+        numerador = leInteiro("Numerador:");
+        
+        // Lê o denominador
+        denominador = leInteiro("Denominador:");
+        
+       try {
                 
-            System.out.printf ("\n\nResultado: %d / %d = %d\n\n", numerador, denominador, quociente (numerador, denominador));
+            System.out.printf (
+                    "\n\nResultado: %d / %d = %d\n\n", 
+                    numerador, 
+                    denominador, 
+                    quocienteComExcecao (numerador, denominador)
+            );
+            
+            System.out.println ("\n\nCódigo executado sem erro!!!\n\n");
 
         } catch (ArithmeticException e) {
                 
